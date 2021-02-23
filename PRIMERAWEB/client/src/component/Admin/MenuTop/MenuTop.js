a2e0fb726d36af13fc3a0b6882f63099b89e7d72
badkluster <78565993+badkluster@users.noreply.github.com>
2021-02-23 20:40:47 -0300

    Home 23/2
    Estable Servidor/Cliente

---
+++ PRIMERAWEB/client/src/component/Admin/MenuTop/MenuTop.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import {
+  MenuUnfoldOutlined,
+  PoweroffOutlined,
+  MenuOutlined,
+  WindowsFilled,
+} from "@ant-design/icons";
+import { logout } from "../../../api/auth";
+
+import "./MenuTop.scss";
+import JorLogo from "../../../assets/img/png/logo sinaptic.png";
+
+export default function MenuTop(props) {
+  const { menuCollapsed, setMenuCollapsed } = props;
+  const logoutUser = () => {
+    logout();
+    window.location.reload();
+  };
+
+  return (
+    <div className="menu-top">
+      <div className="menu.top__left">
+        <Link to={"/admin"}>
+          <img
+            className="menu-top__left-logo"
+            src={JorLogo}
+            alt="Jorge Emanuel Dominguez"
+          ></img>
+        </Link>
+        <Button type="Link" onClick={() => setMenuCollapsed(!menuCollapsed)}>
+          {menuCollapsed ? <MenuOutlined /> : <MenuUnfoldOutlined />}
+        </Button>
+      </div>
+      <div className="menu-top__rigth">
+        <button type="Link" onClick={logoutUser}>
+          <PoweroffOutlined />
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ PRIMERAWEB/client/src/layouts/layoutsBasic.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { Layout } from "antd";
+
+import "./LayoutBasic.scss";
+
+export default function LayoutBasic(props) {
+  const { routes } = props;
+  const { Content, Footer } = Layout;
+
+  return (
+    <Layout>
+      <h1>Menu Home</h1>
+      <Layout>
+        <Content>
+          <LoadRouters routes={routes} />
+        </Content>
+        <Footer> Jorge Dominugez </Footer>
+      </Layout>
+    </Layout>
+  );
+}
+
+function LoadRouters({ routes }) {
+  return (
+    <Switch>
+      {routes.map((route, index) => (
+        <Route
+          key={index}
+          path={route.path}
+          exact={route.exact}
+          component={route.component}
+        />
+      ))}
+    </Switch>
+  );
+}
